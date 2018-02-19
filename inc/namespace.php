3a04156efbc64398c6229e2fed6241d0bfaf4f9f
Dominik Schilling <dominikschilling+git@gmail.com>
2018-02-19 22:28:56 +0100

    Use tabs for indent style

--- inc/namespace.php
+++ inc/namespace.php
@@ -12,10 +12,10 @@ namespace Required\Traduttore_Registry;
  * @param string $api_url Full GlotPress API URL for the project.
  */
 function add_project( $type, $slug, $api_url ) {
-    /**
-     * Short-circuits translations API requests for private projects.
-     */
-    add_filter( 'translations_api', function ( $result, $requested_type, $args ) use ( $type, $slug, $api_url ) {
+	/**
+	 * Short-circuits translations API requests for private projects.
+	 */
+	add_filter( 'translations_api', function ( $result, $requested_type, $args ) use ( $type, $slug, $api_url ) {
 		if ( $type === $requested_type && $slug === $args['slug'] ) {
 			return get_translations( $type, $args['slug'], $api_url );
 		}
@@ -23,25 +23,25 @@ function add_project( $type, $slug, $api_url ) {
 		return $result;
 	}, 10, 3 );
 
-    /**
-     * Filters the translations transients to include the private plugin or theme.
-     */
-    add_filter( 'site_transient_update_' . $type . 's', function ( $value ) use ( $type, $slug, $api_url ) {
-        if ( ! isset( $value->translations ) ) {
-            $value->translations = [];
-        }
+	/**
+	 * Filters the translations transients to include the private plugin or theme.
+	 */
+	add_filter( 'site_transient_update_' . $type . 's', function ( $value ) use ( $type, $slug, $api_url ) {
+		if ( ! isset( $value->translations ) ) {
+			$value->translations = [];
+		}
 
-        $translations = get_translations( $type, $slug, $api_url );
+		$translations = get_translations( $type, $slug, $api_url );
 
-        foreach ( (array) $translations['translations'] as $translation ) {
-            $translation['type'] = $type;
-            $translation['slug'] = $slug;
+		foreach ( (array) $translations['translations'] as $translation ) {
+			$translation['type'] = $type;
+			$translation['slug'] = $slug;
 
-            $value->translations[] = $translation;
-        }
+			$value->translations[] = $translation;
+		}
 
-        return $value;
-    } );
+		return $value;
+	} );
 }
 
 /**
@@ -56,12 +56,12 @@ function add_project( $type, $slug, $api_url ) {
  * @return array Translation data.
  */
 function get_translations( $type, $slug, $url ) {
-    $transient = $type . '_translations_' . $slug;
+	$transient = $type . '_translations_' . $slug;
 
 	$results = get_site_transient( $transient );
 
 	if ( false === $results ) {
-		$res = json_decode( wp_remote_retrieve_body( wp_remote_get( $url, [ 'timeout'   => 3 ] ) ), true );
+		$res = json_decode( wp_remote_retrieve_body( wp_remote_get( $url, [ 'timeout' => 3 ] ) ), true );
 
 		if ( $res ) {
 			set_site_transient( $transient, $res, HOUR_IN_SECONDS * 12 );
