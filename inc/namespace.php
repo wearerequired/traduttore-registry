182aa56001c946dcbc66a6ea750802f483262992
Pascal Birchler <hello@pascalbirchler.ch>
2018-06-19 17:18:54 +0200

    Format code according to WPCS

--- inc/namespace.php
+++ inc/namespace.php
@@ -17,49 +17,53 @@ function add_project( $type, $slug, $api_url ) {
 	/**
 	 * Short-circuits translations API requests for private projects.
 	 */
-	add_filter( 'translations_api', function ( $result, $requested_type, $args ) use ( $type, $slug, $api_url ) {
-		if ( $type === $requested_type && $slug === $args['slug'] ) {
-			return get_translations( $type, $args['slug'], $api_url );
-		}
+	add_filter(
+		'translations_api', function ( $result, $requested_type, $args ) use ( $type, $slug, $api_url ) {
+			if ( $type === $requested_type && $slug === $args['slug'] ) {
+				return get_translations( $type, $args['slug'], $api_url );
+			}
 
-		return $result;
-	}, 10, 3 );
+			return $result;
+		}, 10, 3
+	);
 
 	/**
 	 * Filters the translations transients to include the private plugin or theme.
 	 *
 	 * @see wp_get_translation_updates()
 	 */
-	add_filter( 'site_transient_update_' . $type . 's', function ( $value ) use ( $type, $slug, $api_url ) {
-		if ( ! $value ) {
-			$value = (object) [];
-		}
-
-		if ( ! isset( $value->translations ) ) {
-			$value->translations = [];
-		}
-
-		$installed_translations = wp_get_installed_translations( $type . 's' );
-		$translations = get_translations( $type, $slug, $api_url );
-
-		foreach ( (array) $translations['translations'] as $translation ) {
-			if ( isset( $installed_translations[ $slug ][ $translation['language'] ] ) && $translation['updated'] ) {
-				$local  = new DateTime( $installed_translations[ $slug ][ $translation['language'] ]['PO-Revision-Date'] );
-				$remote = new DateTime( $translation['updated'] );
-
-				if ( $local >= $remote ) {
-					continue;
-				}
+	add_filter(
+		'site_transient_update_' . $type . 's', function ( $value ) use ( $type, $slug, $api_url ) {
+			if ( ! $value ) {
+				$value = (object) [];
 			}
 
-			$translation['type']    = $type;
-			$translation['slug']    = $slug;
+			if ( ! isset( $value->translations ) ) {
+				$value->translations = [];
+			}
 
-			$value->translations[] = $translation;
+			$installed_translations = wp_get_installed_translations( $type . 's' );
+			$translations           = get_translations( $type, $slug, $api_url );
+
+			foreach ( (array) $translations['translations'] as $translation ) {
+				if ( isset( $installed_translations[ $slug ][ $translation['language'] ] ) && $translation['updated'] ) {
+					$local  = new DateTime( $installed_translations[ $slug ][ $translation['language'] ]['PO-Revision-Date'] );
+					$remote = new DateTime( $translation['updated'] );
+
+					if ( $local >= $remote ) {
+						continue;
+					}
+				}
+
+				$translation['type'] = $type;
+				$translation['slug'] = $slug;
+
+				$value->translations[] = $translation;
+			}
+
+			return $value;
 		}
-
-		return $value;
-	} );
+	);
 }
 
 /**
