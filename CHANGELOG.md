a52111c006f504842055f20efc80c2f899a135ec
Pascal Birchler <hello@pascalbirchler.ch>
2018-09-05 17:57:25 +0200

    Begin with tests setup
    See #8.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.2
+
+* Fixed: Improves support for new `wp language plugin|theme install|update` commands in WP-CLI 2.0. 
+
 ## 1.0.1
 
 * Fixed: Prevents a PHP notice that could occur under some circumstances. ([#6](https://github.com/wearerequired/traduttore-registry/issues/6))
---
+++ tests/phpunit/tests/GetTranslations.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class GetTranslations
+ *
+ * @package Traduttore\Tests
+ */
+namespace Required\Traduttore_Registry\Tests;
+
+use \WP_UnitTestCase;
+
+/**
+ *  Test cases for the get_translations() function.
+ */
+class GetTranslations extends WP_UnitTestCase {
+	public function test_plugin_translations() {
+		$this->markTestSkipped('This test has not been implemented yet.');
+	}
+}
