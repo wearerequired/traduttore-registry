bb33e6e66719c907927ef4e6abb2b6175927179f
Pascal Birchler <hello@pascalbirchler.ch>
2018-09-06 11:45:01 +0200

    Add some basic tests

---
+++ tests/phpunit/tests/AddProject.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Class AddProject
+ *
+ * @package Traduttore\Tests
+ */
+
+namespace Required\Traduttore_Registry\Tests;
+
+use \WP_UnitTestCase;
+use function \Required\Traduttore_Registry\add_project;
+
+/**
+ *  Test cases for the add_project() function.
+ */
+class AddProject extends WP_UnitTestCase {
+	public function test_filter_translation_api() {
+		require_once ABSPATH . 'wp-admin/includes/translation-install.php';
+
+		$type = 'plugin';
+		$slug = 'test-plugin';
+		$api_url  = 'https://translate.required.com/api/translations/required/test-plugin/';
+		$expected = [ 'foo' => 'bar' ];
+
+		add_project( $type, $slug, $api_url );
+
+		add_filter(
+			'pre_http_request',
+			function ( $result, $args, $url ) use ( $api_url, $expected ) {
+				if ( $api_url === $url ) {
+					return [ 'headers' => [], 'body' => json_encode( $expected ), 'response' => [] ];
+				}
+
+				return $result;
+			},
+			10,
+			3
+		);
+
+		$actual = translations_api( 'plugins', [ 'slug' => $slug ] );
+
+		$this->assertSame( $expected, $actual );
+	}
+}
--- tests/phpunit/tests/GetTranslations.php
+++ tests/phpunit/tests/GetTranslations.php
@@ -4,15 +4,60 @@
  *
  * @package Traduttore\Tests
  */
+
 namespace Required\Traduttore_Registry\Tests;
 
 use \WP_UnitTestCase;
+use function \Required\Traduttore_Registry\get_translations;
 
 /**
  *  Test cases for the get_translations() function.
  */
 class GetTranslations extends WP_UnitTestCase {
 	public function test_plugin_translations() {
-		$this->markTestSkipped('This test has not been implemented yet.');
+		$api_url  = 'https://translate.required.com/api/translations/required/foo-plugin/';
+		$expected = [ 'foo' => 'bar' ];
+
+
+		add_filter(
+			'pre_http_request',
+			function ( $result, $args, $url ) use ( $api_url, $expected ) {
+				if ( $api_url === $url ) {
+					return [ 'headers' => [], 'body' => json_encode( $expected ), 'response' => [] ];
+				}
+
+				return $result;
+			},
+			10,
+			3
+		);
+
+		$actual = get_translations( 'plugin', 'fo-plugin', $api_url );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function test_data_is_stored_in_transient() {
+		$api_url  = 'https://translate.required.com/api/translations/required/bar-plugin/';
+		$expected = [ 'foo' => 'bar' ];
+
+		add_filter(
+			'pre_http_request',
+			function ( $result, $args, $url ) use ( $api_url, $expected ) {
+				remove_filter( 'pre_http_request', __FUNCTION__ );
+
+				if ( $api_url === $url ) {
+					return [ 'headers' => [], 'body' => json_encode( $expected ), 'response' => [] ];
+				}
+
+				return $result;
+			},
+			10,
+			3
+		);
+
+		get_translations( 'plugin', 'bar-plugin', $api_url );
+
+		self::assertNotFalse( get_site_transient( 'plugin_translations_bar-plugin' ) );
 	}
 }
