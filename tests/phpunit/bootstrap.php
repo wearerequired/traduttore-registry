a4ef4f343483a132e577af06b2424d9623e8fcde
Pascal Birchler <pascalb@google.com>
2019-06-26 11:11:43 +0200

    Tests Bootstrap: Warn when autoload file is missing

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -16,6 +16,13 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	exit( 1 );
 }
 
+$_root_dir = dirname( __DIR__, 2  );
+
+if ( ! file_exists( dirname( __DIR__, 2  ). '/vendor/autoload.php' ) ) {
+	echo "Could not find $_root_dir/vendor/autoload.php, have you run composer install ?" . PHP_EOL;
+	exit( 1 );
+}
+
 putenv( "WP_TESTS_DIR=$_tests_dir" );
 
 // Start up the WP testing environment.
